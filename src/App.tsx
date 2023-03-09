ca50a5d5c0c81193bbe5a5e7e860fb8c879be4b5
EV1L46rus <vislik46rus@gmail.com>
2023-03-09 16:24:31 +0300

    build

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import './App.css';
+import { useAppSelector } from './store/hooks/redux';
+
+function App() {
+  
+  return (
+    <div className="App">
+      
+    </div>
+  );
+}
+
+export default App;
